50173cd5c47e08eeca1a9b10e8300654dd07afa6
Abhisekh <abhisekhmishra26@gmail.com>
2022-06-18 12:40:37 +0530

    ...

--- src/com/company/Maze.java
+++ src/com/company/Maze.java
@@ -1,10 +1,13 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Maze {
     public static void main(String[] args) {
-        System.out.println(countPaths(3,3));
-        System.out.println();
-        printPaths("",3,3);
+//        System.out.println(countPaths(3,3));
+//        System.out.println();
+//        printPaths("",3,3);
+        System.out.println(printPathsRet("",3,3));
     }
     static int countPaths(int r, int c){
 
@@ -28,5 +31,20 @@ public class Maze {
         if (c > 1){
             printPaths(p+'R',r,c-1);
         }
+    }
+    static ArrayList<String> printPathsRet(String p, int r, int c){
+        if (r == 1 && c == 1){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        ArrayList<String> list = new ArrayList<>();
+        if (r > 1){
+            list.addAll(printPathsRet(p+'D',r-1,c));
+        }
+        if (c > 1){
+            list.addAll(printPathsRet(p+'R',r,c-1));
+        }
+        return list;
     }
 }
