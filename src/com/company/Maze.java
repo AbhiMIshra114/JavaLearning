9e9cc4f6ae95e91cc7dbd5ff724f81b0afbb7992
Abhisekh <abhisekhmishra26@gmail.com>
2022-06-18 19:55:46 +0530

    new file added

--- src/com/company/Maze.java
+++ src/com/company/Maze.java
@@ -7,8 +7,19 @@ public class Maze {
 //        System.out.println(countPaths(3,3));
 //        System.out.println();
 //        printPaths("",3,3);
-        System.out.println(printPathsRet("",3,3));
+//        System.out.println(printPathsRet("",3,3));
+//        System.out.println(countPathsDiagonal(3,3));
+//        System.out.println(countPathMazeWithObstacle(0,0,3,3));
+        boolean[][] board = {
+                {true,true,true},
+                {true,false,true},
+                {true,true,true}
+        };
+        pathRestrictions("",board,0,0);
+
     }
+
+
     static int countPaths(int r, int c){
 
         if (r == 1 || c == 1)
@@ -19,6 +30,19 @@ public class Maze {
         // count paths rightwards
         int rightPaths = countPaths(r,c-1);
         return downPaths+rightPaths;
+    }
+    static int countPathsDiagonal(int r, int c){
+
+        if (r == 1 || c == 1)
+            return 1;
+
+        // count paths downwards
+        int downPaths = countPathsDiagonal(r-1,c);
+        // count paths rightwards
+        int rightPaths = countPathsDiagonal(r,c-1);
+        // count paths diagonal
+        int diagPaths = countPathsDiagonal(r-1,c-1);
+        return downPaths+rightPaths+diagPaths;
     }
     static void printPaths(String p, int r, int c){
         if (r == 1 && c == 1){
@@ -46,5 +70,31 @@ public class Maze {
             list.addAll(printPathsRet(p+'R',r,c-1));
         }
         return list;
+    }
+    static int countPathMazeWithObstacle(int r, int c, int R, int C){
+        if (r==R-1 || c==C-1) {
+            return 1;
+        }
+        if (r==1 && c==1) {
+            return 0;
+        }
+        int downPath = countPathMazeWithObstacle(r+1,c,R,C);
+        int rightPath = countPathMazeWithObstacle(r,c+1,R,C);
+        return downPath + rightPath;
+    }
+    static void pathRestrictions(String p, boolean[][] maze, int r, int c){
+        if (r == maze.length-1 && c == maze[0].length-1){
+            System.out.println(p);
+            return;
+        }
+        if (!maze[r][c]){
+            return;
+        }
+        if (r < maze.length-1){
+            pathRestrictions(p+'D',maze,r+1,c);
+        }
+        if (c < maze.length-1){
+            pathRestrictions(p+'R',maze,r,c+1);
+        }
     }
 }
