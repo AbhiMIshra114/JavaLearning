9bd10d10b0d68cd958bf3bfd98540b629ad9134f
Abhisekh <abhisekhmishra26@gmail.com>
2022-07-24 21:42:07 +0530

    add

--- src/linkedList/Main.java
+++ src/linkedList/Main.java
@@ -4,8 +4,16 @@ import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args) {
-        LinkedList<Integer> list = new LinkedList<>();
-        list.add(23);
-        System.out.println(list);
+        LL list = new LL();
+        list.insertFirst(12);
+        list.insertFirst(45);
+        list.insertFirst(23);
+        list.insertFirst(18);
+        list.insertFirst(10);
+
+        list.insertLast(99);
+        list.display();
+
+
     }
 }
